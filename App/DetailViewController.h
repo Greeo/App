fd0291b708b6960aeab9514505eaaa3ecbf07bda
Omar Greene <ogreene53@omars-imac.westell.com>
2012-05-21 04:26:21 -0400

    Initial Commit

---
+++ App/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  App
+//
+//  Created by Tanisha Greene on 5/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ AppTests/AppTests.h
@@ -0,0 +1,13 @@
+//
+//  AppTests.h
+//  AppTests
+//
+//  Created by Tanisha Greene on 5/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AppTests : SenTestCase
+
+@end
